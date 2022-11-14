65707c3b90788a05dd89e5742abcae92c92d03f1
Hanmin Park <hanmpark@student.42nice.fr>
2022-11-14 14:00:22 +0100

    Substr
    Still have an issue with the number of bytes allocated
    cf : libftTester --> 2nd test == MKO

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 15:31:11 by hanmpark          #+#    #+#             */
-/*   Updated: 2022/11/12 16:40:40 by hanmpark         ###   ########.fr       */
+/*   Updated: 2022/11/14 13:59:31 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,36 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*dest;
-	size_t	i;
+	char			*dest;
+	unsigned int	i;
 
 	if (!s)
 		return (NULL);
-	i = 0;
-	dest = malloc((len + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
-	if (start < ft_strlen(s))
+	if (start > ft_strlen(s))
 	{
-		while (s[start + i] && i < len)
-		{
-			dest[i] = s[start + i];
-			i++;
-		}
+		if (!(dest = malloc(1 * sizeof(char))))
+			return (NULL);
+		*dest = 0;
+		return (dest);
 	}
-	dest[start + i] = '\0';
+	if (!(dest = malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = 0;
 	return (dest);
 }
+/*#include <stdio.h>
+int	main()
+{
+	char	str[] = "lorem ipsum dolor sit amet";
+	char	*strsub;
+
+	strsub = ft_substr(str, 400, 20);
+	printf("%s\n", strsub);
+	return (0);
+}*/
