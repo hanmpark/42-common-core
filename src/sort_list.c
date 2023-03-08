31ed27b7c39c844b12a48354b15104bad14d89a3
Hanmin Park <hanmpark@student.42nice.fr>
2023-03-08 16:33:36 +0100

    Nothing big

--- src/sort_list.c
+++ src/sort_list.c
@@ -6,7 +6,7 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:31:37 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/03/08 10:21:52 by hanmpark         ###   ########.fr       */
+/*   Updated: 2023/03/08 11:15:29 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 void	sort_list(t_data *data)
 {
-	rotate_a(&data->a);
-	push_b(&data->a, &data->b);
-	push_b(&data->a, &data->b);
-	push_b(&data->a, &data->b);
-	rotate_both(&data->a, &data->b);
+	
 }
