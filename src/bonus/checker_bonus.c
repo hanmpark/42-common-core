8401f9900241b201925f786115c71d011690de04
Hanmin Park <hanmpark@student.42nice.fr>
2023-03-17 19:27:07 +0100

    Deleted the useless comment 📝

--- src/bonus/checker_bonus.c
+++ src/bonus/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 08:23:25 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/03/17 16:38:07 by hanmpark         ###   ########.fr       */
+/*   Updated: 2023/03/17 19:22:38 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,37 +42,6 @@ static int	check_instruction(const char *arg, t_data *data)
 	}
 	return (NOT_READABLE);
 }
-
-// if (!ft_strncmp(arg, "sa\n", ft_strlen(arg)))
-// 	swap_list(&data->a);
-// else if (!ft_strncmp(arg, "sb\n", ft_strlen(arg)))
-// 	swap_list(&data->b);
-// else if (!ft_strncmp(arg, "ss\n", ft_strlen(arg)))
-// 	swap_both(&data->a, &data->b);
-// else if (!ft_strncmp(arg, "pa\n", ft_strlen(arg)))
-// 	push_list(&data->b, &data->a);
-// else if (!ft_strncmp(arg, "pb\n", ft_strlen(arg)))
-// 	push_list(&data->a, &data->b);
-// else if (!ft_strncmp(arg, "ra\n", ft_strlen(arg)))
-// 	rotate_list(&data->a);
-// else if (!ft_strncmp(arg, "rb\n", ft_strlen(arg)))
-// 	rotate_list(&data->b);
-// else if (!ft_strncmp(arg, "rr\n", ft_strlen(arg)))
-// 	rotate_both(&data->a, &data->b);
-// else if (!ft_strncmp(arg, "rra\n", ft_strlen(arg)))
-// 	reverse_rotate_list(&data->a);
-// else if (!ft_strncmp(arg, "rrb\n", ft_strlen(arg)))
-// 	reverse_rotate_list(&data->b);
-// else if (!ft_strncmp(arg, "rrr\n", ft_strlen(arg)))
-// 	reverse_rotate_both(&data->a, &data->b);
-// else
-// {
-// 	if (data->a)
-// 		ft_lstclear(&data->a);
-// 	if (data->b)
-// 		ft_lstclear(&data->b);
-// 	ft_error("Error\n");
-// }
 
 static void	wait_instructions(t_data *data)
 {
